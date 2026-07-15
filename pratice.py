71173754e8eb2578442f32106000f6601e73d766
Emmanuel Gyekye <gyekye061@gmail.com>
2026-07-15 00:53:20 +0000

    Updated project

---
+++ pratice.py
@@ -0,0 +1,56 @@
+int1 = 24 # ADDITION
+int2 =65
+sum_int = int1 + int2
+print(sum_int)
+my_int1 = 32   # MULTIPLICATION
+my_int2 = 23
+product_ints = my_int1 * my_int2
+print(product_ints)
+int_1 = 64    # DIVISION
+int_2 = 5
+division_ints = int_1 / int_2
+modulo_ints = int_1 % int_2
+expo_ints = int_1 ** int_2
+print(division_ints)
+print(modulo_ints)
+print(expo_ints)
+nn = 2.3346
+mm = 3.097
+round_nn = round(nn, 2)
+round_mm = round(mm, 2)
+print(round_nn)
+print(round_mm)
+nums = -34
+absolute_num = abs(nums)
+print(absolute_num)
+# IF ELSE-IFs
+num = 4
+if num == 1:
+    print("Today is Monday")
+elif num == 2:
+    print("Today is Tuesday")
+elif num == 3:
+    print("Today is Wednesday")
+elif num == 4:
+    print("Today is Thursday")
+elif num == 5:
+    print("Today is Friday")
+elif num == 6:
+    print("Today is Saturday")
+elif num == 7:
+    print("Today is Sunday")
+else:
+    print("Invalid")
+
+
+
+    # GRADES
+score = 43
+if score >= 80:
+        print("Grade A")
+elif score >= 75:
+        print("Grade B")
+elif score >= 50:
+        print("Grade C")
+else:
+        print("Fail")
